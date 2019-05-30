3cc059af814061cb037677a964bf3e1f08436f23
ilvli <995569374@qq.com>
2019-05-30 22:56:06 +0800

    创建了sqlite的操作类
    创建了sqlite的操作类，文件名为Sqlite_Operate_Function

---
+++ maininterface_1/WarehouseManagementSystem1/WarehouseManagementSystem1/Sqlite_Operate_Function.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseManagementSystem1
+{
+    class Sqlite_Operate_Function
+    {
+    }
+}
